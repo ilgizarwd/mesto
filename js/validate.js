bb7f070fb7a4e72d69a71142caf84b757e6c0fc4
Ilgizar Shaimardanov <ilgizar2000@yandex.ru>
2022-06-30 17:57:06 +0300

    6start_val

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,8 @@
-
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button-save',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
